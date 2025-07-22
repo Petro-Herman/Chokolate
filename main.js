84666a8fa05f5a1346e2e14962702a07163611de
Petro-Herman <germanpetr24@gmail.com>
2025-07-22 23:59:10 +0300

    modal+slide

---
+++ main.js
@@ -0,0 +1,79 @@
+const openBtn = document.querySelector(".loved-btn");
+const closeBtn = document.querySelector(".modal-close");
+const modal = document.querySelector(".backdrop");
+
+function openModal() {
+  modal.classList.add("is-open");
+  document.body.classList.add("modal-open");
+}
+
+function closeModal() {
+  modal.classList.remove("is-open");
+  document.body.classList.remove("modal-open");
+}
+
+openBtn.addEventListener("click", openModal);
+modal.addEventListener("click", closeModal);
+closeBtn.addEventListener("click", closeModal);
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeModal();
+  }
+});
+
+// const openMenuBtn = document.querySelector(".mob-menu-btn");
+// const closeMenuBtn = document.querySelector(".mob-menu-close");
+// const mobMenu = document.querySelector(".mob-menu");
+// const menuLinks = document.querySelectorAll(".mob-item-link .mob-nav-item a");
+
+// function openMenu() {
+//   mobMenu.classList.add("is-open");
+//   document.body.classList.add("modal-open");
+// }
+
+// function closeMenu() {
+//   mobMenu.classList.remove("is-open");
+//   document.body.classList.remove("modal-open");
+// }
+
+// openMenuBtn.addEventListener("click", openMenu);
+// closeMenuBtn.addEventListener("click", closeMenu);
+
+// menuLinks.forEach((link) => {
+//   link.addEventListener("click", closeMenu);
+// });
+
+// mobMenu.addEventListener("click", (e) => {
+//   if (!e.target.closest(".mob-menu-wrap")) {
+//     closeMenu();
+//   }
+// });
+const openMenuBtn = document.querySelector(".mob-menu-btn");
+const closeMenuBtn = document.querySelector(".mob-menu-close");
+const mobMenu = document.querySelector(".mob-menu");
+const menuLinks = document.querySelectorAll(".mob-item-link, .mob-nav-item a");
+
+function openMenu() {
+  mobMenu.classList.add("is-open");
+  document.body.style.overflow = "hidden";
+}
+
+function closeMenu() {
+  mobMenu.classList.remove("is-open");
+  document.body.style.overflow = "";
+}
+
+openMenuBtn.addEventListener("click", openMenu);
+closeMenuBtn.addEventListener("click", closeMenu);
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
+
+// Клік за межами .mob-menu-wrap закриває меню
+mobMenu.addEventListener("click", (event) => {
+  if (!event.target.closest(".mob-menu-wrap")) {
+    closeMenu();
+  }
+});
